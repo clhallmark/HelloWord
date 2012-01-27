a68078ad68c810ba2c4249fb46333f1f4f72de34
C L <estieliu@hotmail.com>
2012-01-27 13:40:53 -0600

    try again

--- HelloWorld/src/test/helloworld/first/HelloWordl.java
+++ HelloWorld/src/test/helloworld/first/HelloWordl.java
@@ -4,5 +4,6 @@ public class HelloWordl {
 	public static void main(String[] args){
 		System.out.println("Hello Wordl!!!");
 		System.out.println("Hello Wordl!!!");
+		System.out.println("Hello Wordl!!!");
 	}
 }
