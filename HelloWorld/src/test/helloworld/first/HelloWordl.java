8ef6bfbac9e43103826341c1e90a29923b43e4d5
C L <estieliu@hotmail.com>
2012-01-27 13:37:24 -0600

    add one line
    Signed-off-by: C L <estieliu@hotmail.com>

--- HelloWorld/src/test/helloworld/first/HelloWordl.java
+++ HelloWorld/src/test/helloworld/first/HelloWordl.java
@@ -3,5 +3,6 @@ package test.helloworld.first;
 public class HelloWordl {
 	public static void main(String[] args){
 		System.out.println("Hello Wordl!!!");
+		System.out.println("Hello Wordl!!!");
 	}
 }
